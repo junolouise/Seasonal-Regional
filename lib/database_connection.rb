63256ffe09a3b934a367f8ec578e4590e827cb99
Jack Willee <jackwillee1995@gmail.com>
2020-03-03 15:09:59 +0000

    extracting database logic

---
+++ lib/database_connection.rb
@@ -0,0 +1,12 @@
+class DatabaseConnection
+  def self.setup(dbname)
+    @connection = PG.connect(dbname: dbname)
+  
+  end
+  def self.connection
+   @connection
+  end 
+   def self.query(string)
+    @connection.exec(string)
+   end 
+end 
