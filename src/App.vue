f394e1bde52bf9978d3d4d3d1669df49184c0d9b
benauld <ben@portobello-consultancy.co.uk>
2020-03-03 12:50:50 +0000

    added second screen conditionally powered by button (initially)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <app-header></app-header>
-    <app-ingredients></app-ingredients>
+    <button v-on:click="component = 'app-ingredients'">Show Ingredients</button>
+    <button v-on:click="component = 'app-menus'">Show Menus</button>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
     <app-footer></app-footer>
   </div>
 </template>
@@ -11,14 +15,18 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import Ingredients from "./components/Ingredients.vue";
+import Menus from "./components/Menus.vue";
 export default {
   components: {
     "app-header": Header,
     "app-footer": Footer,
-    "app-ingredients": Ingredients
+    "app-ingredients": Ingredients,
+    "app-menus": Menus
   },
   data() {
-    return {};
+    return {
+      component: "app-ingredients"
+    };
   }
 };
 </script>
